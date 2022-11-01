675c57c28b0d51c9a3184f16be1e062f9fb23454
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-11-01 18:16:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const colors = [
+  'green',
+  'red',
+  'blue',
+  'yellow',
+  'orange',
+  'purple',
+  'pink',
+  'black',
+  'orange',
+  'hotpink',
+  'springgreen',
+  'gray',
+  'brown',
+  'cyan',
+  'magenta',
+  'teal',
+  'indigo',
+  'violet',
+  'lime',
+  'maroon',
+  'navy',
+  'olive',
+  'silver',
+  'aqua',
+  'fuchsia',
+  'gold'
+]
+
+const colorContainer = document.getElementById('color-container')
+const button = document.getElementById('button')
+
+button.addEventListener('click', () => {
+  let randomColor = colors[Math.floor(Math.random() * colors.length)]
+
+  colorContainer.style.background = randomColor
+
+  const h1 = document.createElement('h1')
+  colorContainer.appendChild(h1)
+
+  const colorText =
+    randomColor.charAt(0).toUpperCase() + randomColor.substring(1)
+  h1.textContent = colorText
+})
+
+//Must fix so only the latest value render at h1
